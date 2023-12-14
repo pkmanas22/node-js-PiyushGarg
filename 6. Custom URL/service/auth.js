082f29595fd9428c27d5c1e21bae9e9500938c38
Manas Kumar Pradhan <pk@manas.com>
2023-12-14 11:21:37 +0530

    jwt (JSON Web Token) added for state less authentication

--- 6. Custom URL/service/auth.js
+++ 6. Custom URL/service/auth.js
@@ -1,12 +1,35 @@
-const sessionIdToUserMap = new Map()
+// const sessionIdToUserMap = new Map()
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id,user)
+// JSON Web Token (JWT)
+const jwt = require("jsonwebtoken")
+const secretKey = "Pkmanas@123$short#url"
+
+function setUser(user) {
+    // sessionIdToUserMap.set(id,user)
+    const payload = {
+        // ...user,
+        _id : user._id,
+        email : user.email,
+    }
+
+    return jwt.sign(payload, secretKey)
 }
 
-function getUser(id, user) {
-    return sessionIdToUserMap.get(id)
+function getUser(token) {
+    // return sessionIdToUserMap.get(id)
+
+    if (!token) {
+        return null;
+    }
+
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (error) {
+        console.error("Error verifying JWT:", error.message);
+        return null;
+    }
 }
+
 
 module.exports = {
     setUser,
