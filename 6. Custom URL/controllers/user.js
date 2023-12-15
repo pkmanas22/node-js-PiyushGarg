dfa47d988e3801cb13b1a4a3f5131a989335b05c
Manas Kumar Pradhan <pk@manas.com>
2023-12-15 09:14:44 +0530

    cookie and header for authentication

--- 6. Custom URL/controllers/user.js
+++ 6. Custom URL/controllers/user.js
@@ -1,10 +1,10 @@
 const User = require("../models/user")
 const { v4: uuidv4 } = require("uuid")
-const {setUser, getUser} = require ("../service/auth");
+const { setUser, getUser } = require("../service/auth");
 const { use } = require("../routes/user");
 
-async function handleUserSignup(req,res) {
-    const {name, email, password} = req.body;
+async function handleUserSignup(req, res) {
+    const { name, email, password } = req.body;
     await User.create({
         name,
         email,
@@ -13,18 +13,38 @@ async function handleUserSignup(req,res) {
     return res.redirect("/")
 }
 
-async function handleUserLogin(req,res) {
-    const { email, password} = req.body;
-    const user = await User.findOne({email, password})
-    if(!user) 
-        return res.render("login", {
-            error: "Invalid Username or password"
-        })
-    
-    /*const sessionId = uuidv4()*/
-    const token = setUser(user)
-    res.cookie("uid", token)
-    return res.redirect("/")
+async function handleUserLogin(req, res) {
+    // console.log(req);
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email, password })
+
+        console.log("User query:", { email, password });
+        console.log("Database response:", user);
+
+        if (!user) {
+            console.log("User not found");
+            return res.render("login", { error: "Invalid Username or password" });
+        }
+        /*const sessionId = uuidv4()*/
+
+        // res.cookie("uid", token)
+        /*
+        With specific domain
+        res.cookie("uid", token, {
+            domain: ".piyushgarg.dev"  // www.piyushgarg.dev (or) blog.piyushgarg.dev, etc...
+        })*/
+        // return res.redirect("/")
+
+        const token = setUser(user);
+        // console.log(token);
+        return res.json({ token });
+    } catch (error) {
+        console.error("Error during login:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+
 }
 
 module.exports = {
--- 6. Custom URL/middlewares/auth.js
+++ 6. Custom URL/middlewares/auth.js
@@ -2,13 +2,18 @@ const { getUser } = require("../service/auth")
 
 async function restrictToLoggedinUserOnly(req, res, next) {
     // console.log(req);
-    const userUID = req.cookies?.uid;
+    // const userUID = req.cookies?.uid;  // cookie purpose
+    
+    const userUID = req.headers["Authorization"];  // authorization purpose
 
     if (!userUID) {
         return res.redirect('/login')
     }
 
-    const user = getUser(userUID)
+    // verify the token
+    const token = userUID.split("Bearer ")[1];  // Bearer {fe4df6jhfdejf}
+
+    const user = getUser(token)
 
     if (!user) {
         return res.redirect('/login')
@@ -19,9 +24,16 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-    const userUID = req.cookies?.uid;
+    // const userUID = req.cookies?.uid;
 
-    const user = getUser(userUID)
+    const userUID = req.headers["authorization"];  // authorization purpose
+
+    // verify the token
+    const token = userUID.split("Bearer ")[1];  // Bearer {fe4df6jhfdejf}
+
+    // const user = getUser(userUID)
+
+    const user = getUser(token)
 
     req.user = user;
     next()
