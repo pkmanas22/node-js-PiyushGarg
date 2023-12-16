c469c7af0972ff7335c49d53c71b9b220d9a4899
Manas Kumar Pradhan <pk@manas.com>
2023-12-16 11:57:05 +0530

    image view added

--- 8. Uploading Files/index.js
+++ 8. Uploading Files/index.js
@@ -12,12 +12,12 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads')
     },
-    filename: function (req, file, cb){
-        cb(null, `${Date.now()} - ${file.originalname}`)
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
     }
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 // Configuring EJS view engine
 app.set('view engine', 'ejs');
@@ -27,19 +27,27 @@ app.set('views', path.resolve('./views'));
 app.use(express.json()); // JSON parsing middleware
 app.use(express.urlencoded({ extended: false })) // Form data parsing middleware
 
+// Serve static files from the 'uploads' folder
+app.use('/uploads', express.static('uploads'));
+
+let uploadedFile = null;
+
 // Define your routes and middleware here
 app.post('/profile', upload.single('profileImage'), (req, res) => {
     // console.log(req.body);
     // console.log(req.file);
+    uploadedFile = req.file;
     return res.redirect('/')
 })
 
 // Rendering the 'home' template with URL data
-app.get('/', async (req, res) => {
-  return res.render('homepage');
+app.get('/', (req, res) => {
+    const profileImage = uploadedFile ? `/uploads/${uploadedFile.filename}` : null;
+    console.log(profileImage || 'No file uploaded yet');
+    return res.render('homepage', { profileImage });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+    console.log(`Server started at http://localhost:${PORT}`);
 });
