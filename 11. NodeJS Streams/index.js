b94c4cbeda811dd2e344d983f50cbd61195afe17
Manas Kumar Pradhan <pk@manas.com>
2023-12-19 00:28:39 +0530

    node js stream

---
+++ 11. NodeJS Streams/index.js
@@ -0,0 +1,34 @@
+// Importing required modules
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+const zlib = require('zlib');
+const status = require('express-status-monitor');
+
+// Creating an Express application
+const app = express();
+const PORT = 8000;
+
+app.use(status())
+
+// Stream read (sample.txt)  -> zipper --> fs write stream
+fs.createReadStream('./sample.txt').pipe(zlib.createGzip().pipe(fs.createWriteStream('./sample.zip')))
+
+// Rendering the 'home' template with URL data
+app.get('/', async (req, res) => {
+    // fs.readFile("./sample.txt", (err, data) => {
+    //     res.end(data)
+    // })
+    const stream = fs.createReadStream('./sample.txt', "utf-8")
+    stream.on("data", (chunk) => {
+        res.write(chunk)
+    })
+    stream.on("end", () => {
+        res.end()
+    })
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
